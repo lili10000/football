feff39ee5b63c2e1772d11c2ba8cce8346d5a756
lili10000 <476512727@qq.com>
2018-03-02 14:57:32 +0800

    调整结果显示

---
+++ football.sql
@@ -0,0 +1,10 @@
+create table if not exists k_corner(
+    main VARCHAR(255),
+    client VARCHAR(255),
+    main_score INT,
+    client_score INT,
+    rate VARCHAR(255),
+    type VARCHAR(255),
+    main_corner INT,
+	client_corner INT,
+    sum_corner INT);
