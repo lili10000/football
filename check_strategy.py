2f43add592266849f748d95f078a7c5ce86b30bf
lili10000 <476512727@qq.com>
2018-07-16 15:18:48 +0800

    修正BUG

--- check_strategy.py
+++ check_strategy.py
@@ -12,21 +12,20 @@ class checkStartegy():
     def __init__(self):
         self.startegy = {}
 
+        self.startegy['泰乙']=data(time=80,score=2,small_do="",else_do="买大球")
+        self.startegy['美乙']=data(time=80,score=2,small_do="",else_do="买大球")
+        self.startegy['瑞典超']=data(time=80,score=2,small_do="",else_do="买大球")
+        self.startegy['哈萨甲']=data(time=80,score=2,small_do="",else_do="买大球")
+        self.startegy['里约足联B']=data(time=80,score=2,small_do="",else_do="买大球")
+        self.startegy['日联杯']=data(time=80,score=2,small_do="",else_do="买大球")  
+        self.startegy['越VL']=data(time=80,score=2,small_do="",else_do="买大球")  
 
-        self.startegy['泰乙']=data(time=80,score=2,small_do=None,else_do="买大球")
-        self.startegy['美乙']=data(time=80,score=2,small_do=None,else_do="买大球")
-        self.startegy['瑞典超']=data(time=80,score=2,small_do=None,else_do="买大球")
-        self.startegy['哈萨甲']=data(time=80,score=2,small_do=None,else_do="买大球")
-        self.startegy['里约足联B']=data(time=80,score=2,small_do=None,else_do="买大球")
-        self.startegy['日联杯']=data(time=80,score=2,small_do=None,else_do="买大球")  
-        self.startegy['越VL']=data(time=80,score=2,small_do=None,else_do="买大球")  
+        self.startegy['哈萨超']=data(time=80,score=0,small_do="",else_do="买大球")
+        self.startegy['美职联']=data(time=80,score=0,small_do="",else_do="买大球")
+        self.startegy['天皇杯']=data(time=80,score=0,small_do="",else_do="买大球")
 
-        self.startegy['哈萨超']=data(time=80,score=0,small_do=None,else_do="买大球")
-        self.startegy['美职联']=data(time=80,score=0,small_do=None,else_do="买大球")
-        self.startegy['天皇杯']=data(time=80,score=0,small_do=None,else_do="买大球")
-
-        self.startegy['日职乙']=data(time=80,score=3,small_do="买小球",else_do=None)
-        self.startegy['日职联']=data(time=80,score=3,small_do="买小球",else_do=None)
+        self.startegy['日职乙']=data(time=80,score=3,small_do="买小球",else_do="")
+        self.startegy['日职联']=data(time=80,score=3,small_do="买小球",else_do="")
 
     
 
@@ -48,7 +47,7 @@ class checkStartegy():
             return 
         tmp = self.startegy[type]
         if time < tmp.time:
-            return
+            return ""
         if score < tmp.score:
             return tmp.small_do
         else:
