75b3c0842b7c2ebad923416608f7cf57da1bef40
lili <476512727@qq.com>
2018-07-16 23:04:44 +0800

    1

--- check_strategy.py
+++ check_strategy.py
@@ -11,35 +11,36 @@ class data():
 class checkStartegy():
     def __init__(self):
         self.startegy = {}
+        timeCheck = 75
 
-        self.startegy['泰乙']=data(time=80,score=2,small_do="",else_do="买大球")
-        self.startegy['美乙']=data(time=80,score=2,small_do="",else_do="买大球")
-        self.startegy['哈萨甲']=data(time=80,score=2,small_do="",else_do="买大球")
-        self.startegy['里约足联B']=data(time=80,score=2,small_do="",else_do="买大球")
-        self.startegy['日联杯']=data(time=80,score=2,small_do="",else_do="买大球")  
-        self.startegy['越VL']=data(time=80,score=2,small_do="",else_do="买大球")  
-        self.startegy['韩甲挑']=data(time=80,score=2,small_do="",else_do="买大球")  
-        self.startegy['冰岛女甲']=data(time=80,score=2,small_do="",else_do="买大球")  
-        self.startegy['智利乙']=data(time=80,score=2,small_do="",else_do="买大球")  
-        self.startegy['中超']=data(time=80,score=2,small_do="",else_do="买大球")  
-
-
-        self.startegy['印IFA盾']=data(time=80,score=1,small_do="",else_do="买大球")  
-        self.startegy['韩国女K']=data(time=80,score=1,small_do="",else_do="买大球")  
+        self.startegy['泰乙']=data(time=timeCheck,score=2,small_do="",else_do="买大球")
+        self.startegy['美乙']=data(time=timeCheck,score=2,small_do="",else_do="买大球")
+        self.startegy['哈萨甲']=data(time=timeCheck,score=2,small_do="",else_do="买大球")
+        self.startegy['里约足联B']=data(time=timeCheck,score=2,small_do="",else_do="买大球")
+        self.startegy['日联杯']=data(time=timeCheck,score=2,small_do="",else_do="买大球")  
+        self.startegy['越VL']=data(time=timeCheck,score=2,small_do="",else_do="买大球")  
+        self.startegy['韩甲挑']=data(time=timeCheck,score=2,small_do="",else_do="买大球")  
+        self.startegy['冰岛女甲']=data(time=timeCheck,score=2,small_do="",else_do="买大球")  
+        self.startegy['智利乙']=data(time=timeCheck,score=2,small_do="",else_do="买大球")  
+        self.startegy['中超']=data(time=timeCheck,score=2,small_do="",else_do="买大球")  
 
 
-        self.startegy['瑞典北甲']=data(time=80,score=0,small_do="",else_do="买大球")
-        self.startegy['哈萨超']=data(time=80,score=0,small_do="",else_do="买大球")
-        self.startegy['美职联']=data(time=80,score=0,small_do="",else_do="买大球")
-        self.startegy['天皇杯']=data(time=80,score=0,small_do="",else_do="买大球")
-        self.startegy['爱沙杯']=data(time=80,score=0,small_do="",else_do="买大球")
+        self.startegy['印IFA盾']=data(time=timeCheck,score=1,small_do="",else_do="买大球")  
+        self.startegy['韩国女K']=data(time=timeCheck,score=1,small_do="",else_do="买大球")  
+
+
+        self.startegy['瑞典北甲']=data(time=timeCheck,score=0,small_do="",else_do="买大球")
+        self.startegy['哈萨超']=data(time=timeCheck,score=0,small_do="",else_do="买大球")
+        self.startegy['美职联']=data(time=timeCheck,score=0,small_do="",else_do="买大球")
+        self.startegy['天皇杯']=data(time=timeCheck,score=0,small_do="",else_do="买大球")
+        self.startegy['爱沙杯']=data(time=timeCheck,score=0,small_do="",else_do="买大球")
         
-        self.startegy['英友谊']=data(time=75,score=0,small_do="",else_do="买大球")
+        self.startegy['英友谊']=data(time=timeCheck,score=0,small_do="",else_do="买大球")
         
-        self.startegy['苏联杯']=data(time=75,score=3,small_do="买小球",else_do="")
+        self.startegy['苏联杯']=data(time=80,score=3,small_do="买小球",else_do="")
 
         # 黑名单就是啥也不干
-        blackList = data(time=80,score=0,small_do="",else_do="") 
+        blackList = data(time=timeCheck,score=0,small_do="",else_do="") 
 
         self.startegy['日职丙']=blackList
         self.startegy['日足联']=blackList
