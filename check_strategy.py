130d0ce3ad297b758e6dcbb829d448fc8da6cb2f
lili10000 <476512727@qq.com>
2018-07-16 17:20:10 +0800

    添加联赛个性化策略

--- check_strategy.py
+++ check_strategy.py
@@ -14,20 +14,34 @@ class checkStartegy():
 
         self.startegy['泰乙']=data(time=80,score=2,small_do="",else_do="买大球")
         self.startegy['美乙']=data(time=80,score=2,small_do="",else_do="买大球")
-        self.startegy['瑞典超']=data(time=80,score=2,small_do="",else_do="买大球")
         self.startegy['哈萨甲']=data(time=80,score=2,small_do="",else_do="买大球")
         self.startegy['里约足联B']=data(time=80,score=2,small_do="",else_do="买大球")
         self.startegy['日联杯']=data(time=80,score=2,small_do="",else_do="买大球")  
         self.startegy['越VL']=data(time=80,score=2,small_do="",else_do="买大球")  
+        self.startegy['韩甲挑']=data(time=80,score=2,small_do="",else_do="买大球")  
+        self.startegy['冰岛女甲']=data(time=80,score=2,small_do="",else_do="买大球")  
+        self.startegy['智利乙']=data(time=80,score=2,small_do="",else_do="买大球")  
+        self.startegy['中超']=data(time=80,score=2,small_do="",else_do="买大球")  
 
+
+        self.startegy['印IFA盾']=data(time=80,score=1,small_do="",else_do="买大球")  
+        self.startegy['韩国女K']=data(time=80,score=1,small_do="",else_do="买大球")  
+
+
+        self.startegy['瑞典北甲']=data(time=80,score=0,small_do="",else_do="买大球")
         self.startegy['哈萨超']=data(time=80,score=0,small_do="",else_do="买大球")
         self.startegy['美职联']=data(time=80,score=0,small_do="",else_do="买大球")
         self.startegy['天皇杯']=data(time=80,score=0,small_do="",else_do="买大球")
-
-
+        self.startegy['爱沙杯']=data(time=80,score=0,small_do="",else_do="买大球")
+        
+        self.startegy['英友谊']=data(time=75,score=0,small_do="",else_do="买大球")
+        
+        self.startegy['苏联杯']=data(time=75,score=3,small_do="买小球",else_do="")
 
         # 黑名单就是啥也不干
         blackList = data(time=80,score=0,small_do="",else_do="") 
+
+        self.startegy['日职丙']=blackList
         self.startegy['日足联']=blackList
         self.startegy['赞比亚超']=blackList
         self.startegy['巴圣SB']=blackList
@@ -36,6 +50,16 @@ class checkStartegy():
         self.startegy['拉脱V']=blackList
         self.startegy['韩国K2']=blackList
         self.startegy['韩K联']=blackList
+        self.startegy['印尼超']=blackList
+        self.startegy['新加坡U19']=blackList
+        self.startegy['瑞典超']=blackList
+        self.startegy['欧锦赛 U19']=blackList
+        self.startegy['丹超']=blackList
+        self.startegy['冰岛超']=blackList
+        self.startegy['厄瓜锦']=blackList
+        self.startegy['欧罗巴']=blackList
+        
+        
         
         
 
